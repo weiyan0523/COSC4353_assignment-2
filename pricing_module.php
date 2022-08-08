3a47fe8aa8e2c2dded08eac30022586a698d6202
Wei Yan <wyan6@cougarnet.uh.edu>
2022-08-07 20:04:37 -0500

    Add files via upload

---
+++ pricing_module.php
@@ -0,0 +1,16 @@
+<?php
+$per_gallon = 1.5;
+$out_state = 0.04;
+$in_state = 0.02;
+
+$with_history = 0.01;
+$no_history = 0;
+$profit = 0.1;
+$gallons_1000 = 0.02;
+$gallons_less = 0.03;
+
+$total = 0;
+$sug = 0;
+
+
+?>
